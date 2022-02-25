b22ddafea94a121defa47b8a68f0d9aa74de5eb9
ARYAN PATEL <patelaryan7751@gmail.com>
2022-02-25 15:58:53 +0530

    Update signals.py

--- tasks/signals.py
+++ tasks/signals.py
@@ -2,8 +2,6 @@ from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from tasks.models import Task, TaskEmail, TaskHistory, User
 
-# signal for storing report schedule for a user
-
 
 @receiver(post_save, sender=User)
 def mailSchedule(sender, instance, created, **kwargs):
